0b31917288729b960999b778225e270e3c2e5b77
1amhellboy <panwarlakshyadaksh@gmail.com>
2025-09-09 15:40:26 +0000

    logout/request-reset/reset-passwaord api added

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -52,8 +52,15 @@ export const loginUser = async(email:string,password:string)=>{
         body:JSON.stringify({email,password}),
     });
 
+    const data = await res.json();
+
     if(!res.ok) throw new Error("Login failed");
-    return res.json();
+    // return res.json();
+
+    // Save tokens locally
+    localStorage.setItem("accessToken",data.accessToken);
+    localStorage.setItem("refreshToken",data.refreshToken);
+    return data;
 }
 
 
@@ -61,3 +68,70 @@ export const loginUser = async(email:string,password:string)=>{
 // /auth/register (to register a new user)
 // /auth/login (to log in an existing user)
 // They send the email & password to your backend API using fetch() and get a response — like “user created” or “token for login”.
+
+// Logout
+export const logoutUser = async() => {
+  const refreshToken = localStorage.getItem("refreshToken");
+  const res = await fetch(`${API_BASE}/auth/logout`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ refreshToken }),
+  });
+
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
+  
+  if(!res.ok) {
+    throw new Error("Logout failed");
+  }
+
+  return res.json();
+}
+
+// Request password reset
+export const requestPasswordReset = async(email:string)=>{
+  const res = await fetch(`${API_BASE}/auth/request-password-reset`,{
+    method:"POST",
+    headers: { "Content-Type": "application/json" }, 
+    body:JSON.stringify({email}),
+  });
+
+  const data = await res.json();
+
+  if(!res.ok) throw new Error(data.message || data.error ||"Request password reset failed");
+  return data;
+}
+
+
+// Reset password   
+export const resetPassword = async(email:string,newPassword:string,resetToken:string)=>{
+  const res = await fetch(`${API_BASE}/auth/reset-password`,{
+    method:"POST",
+    headers: { "Content-Type": "application/json" }, 
+    body:JSON.stringify({email,newPassword,resetToken}),
+  });
+
+  const data = await res.json();
+
+  if(!res.ok) throw new Error(data.message || data.error ||"Reset password failed");
+  return data;
+}
+
+// Refresh Access Token
+export const refreshAccessToken = async () => {
+  const refreshToken = localStorage.getItem("refreshToken");
+  const res = await fetch(`${API_BASE}/auth/refresh`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ refreshToken }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || data.message || "Failed to refresh token");
+  }
+
+  localStorage.setItem("accessToken", data.accessToken);
+  return data;
+};
